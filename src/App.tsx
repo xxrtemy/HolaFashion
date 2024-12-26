9e57cb60f69fd4431165c3470f3515088463608e
xxrtemy <rebelgamechannel@gmail.com>
2024-12-26 16:27:36 +0300

    Рефактор header'а [ESLint Passed]

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Blog from "./components/blog/Blog";
 import Contacts from "./components/contacts/Contacts";
-import Header from "./components/header/Header";
+import { Header } from "@components/Header";
 import Home from "./components/home/Home";
 import Product from "./components/product/Product"
 import Services from "./components/services/Services.jsx"
