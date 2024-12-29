ba5c429f196d4df377eff4ce9466302afb83fd44
xxrtemy <rebelgamechannel@gmail.com>
2024-12-29 11:00:40 +0300

    Добавлен переиспользуемый компонент Section [ESLint Passed]

---
+++ src/shared/components/Section/index.interface.ts
@@ -0,0 +1,6 @@
+import { PropsWithChildren } from "react"
+
+export interface SectionProps extends PropsWithChildren {
+    heading: string 
+    title: string 
+}
---
+++ src/shared/components/Section/index.style.tsx
@@ -0,0 +1,36 @@
+import styled from "styled-components";
+
+export const SectionWrapper = styled.section`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    background-color: #FFEFE7;
+    padding-bottom: 40px;
+`
+
+export const SectionHeading = styled.h1`
+    margin: 76px 0px 96px 0px;
+    color: var(--bd-957-e, #BD957E);
+    text-align: center;
+    font-size: 32px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 139%; /* 44.48px */
+    letter-spacing: 0.96px;
+`
+
+export const SectionTitle = styled.h2`
+    margin-bottom: 85px;
+    color: #795744;
+    text-align: center;
+    font-family: var(--font-titles);
+    font-size: 48px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 139%; /* 66.72px */
+    letter-spacing: 0.96px;
+`
+
+export const SectionCards = styled.div`
+    margin: 0px 95px 80px 95px;
+`
