c47b342be449a444fd913f905625f8aeb5765321
xxrtemy <rebelgamechannel@gmail.com>
2025-01-31 17:50:03 +0300

    minor fix [ESLint Passed]

---
+++ src/feature/home/consts.tsx
@@ -0,0 +1,14 @@
+
+
+
+export const HOME = {
+    SECTION: {
+        heading: 'HOME',
+        title: 'Hola Fashion - Newest Designers, Lowest Prices',
+        colors: {
+            background: '#FFEFE7',
+            heading: 'var(--bd-957-e, #BD957E)',
+            title:'#795744',
+        }
+    },
+}
