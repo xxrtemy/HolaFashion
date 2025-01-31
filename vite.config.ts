8e143be306da7b4931f942974773538336c767cd
xxrtemy <rebelgamechannel@gmail.com>
2025-01-31 18:57:20 +0300

    fix config [ESLint Passed]

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import svgr from 'vite-plugin-svgr';
 import path from 'path';
 
 export default defineConfig({
-  base: '/',
+  base: '/HolaFashion/',
   plugins: [react(), svgr()],
   resolve: {
     alias: {
